1f0869753f68bc81b3f9c74da74eaedf58b8d52b
陈成 <cheng@tanmer.com>
2019-11-11 11:26:33 +0800

    可在工具块中点击添加；可配置接入iframe

--- src/index.js
+++ src/index.js
@@ -38,18 +38,57 @@ import {debounce} from 'debounce';
  * @property {Object} patterns - static property with patterns for paste handling configuration
  */
 export default class Embed {
+
+  /**
+     * Allow to press Enter inside the LinkTool input
+     * @returns {boolean}
+     * @public
+     */
+    static get enableLineBreaks() {
+      return true;
+    }
+
+  /**
+   * Get Tool toolbox settings
+   * icon - Tool icon's SVG
+   * title - title to show in toolbox
+   *
+   * @return {{icon: string, title: string}}
+   */
+  static get toolbox() {
+    return {
+      icon: `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path fill-rule="evenodd" clip-rule="evenodd" d="M6 4C4.89543 4 4 4.89543 4 6V18C4 19.1046 4.89543 20 6 20H18C19.1046 20 20 19.1046 20 18V6C20 4.89543 19.1046 4 18 4H6ZM18 5H6C5.44772 5 5 5.44772 5 6V18C5 18.5523 5.44772 19 6 19H18C18.5523 19 19 18.5523 19 18V6C19 5.44772 18.5523 5 18 5ZM9.04134 7.65814L16.4752 11.8482L9.04134 16.3761V7.65814ZM10.0413 9.36968V14.5961L14.498 11.8816L10.0413 9.36968Z" fill="#212132"/>
+      </svg>`,
+      title: 'Embed'
+    };
+  }
+
   /**
    * @param {{data: EmbedData, config: EmbedConfig, api: object}}
    *   data — previously saved data
    *   config - user config for Tool
    *   api - Editor.js API
    */
-  constructor({data, api}) {
+  constructor({data, config, api}) {
     this.api = api;
     this._data = {};
     this.element = null;
 
     this.data = data;
+    this.config = config;
+
+    this.nodes = {
+      wrapper: null,
+      container: null,
+      input: null,
+      inputHolder: null,
+      linkContent: null,
+      linkImage: null,
+      linkTitle: null,
+      linkDescription: null,
+      linkText: null
+    };
   }
 
   /**
@@ -74,7 +113,7 @@ export default class Embed {
       embed: embed || this.data.embed,
       width: width || this.data.width,
       height: height || this.data.height,
-      caption: caption || this.data.caption || '',
+      // caption: caption || this.data.caption || '',
     };
 
     const oldView = this.element;
@@ -104,6 +143,9 @@ export default class Embed {
   get CSS() {
     return {
       baseClass: this.api.styles.block,
+      inputEl: 'embed-tool__input',
+      inputHolder: 'embed-tool__input-holder',
+      inputError: 'embed-tool__input-holder--error',
       input: this.api.styles.input,
       container: 'embed-tool',
       containerLoading: 'embed-tool--loading',
@@ -123,6 +165,9 @@ export default class Embed {
     if (!this.data.service) {
       const container = document.createElement('div');
 
+      this.nodes.inputHolder = this.makeInputHolder();
+      container.appendChild(this.nodes.inputHolder);
+
       this.element = container;
 
       return container;
@@ -130,18 +175,18 @@ export default class Embed {
 
     const {html} = Embed.services[this.data.service];
     const container = document.createElement('div');
-    const caption = document.createElement('div');
+    // const caption = document.createElement('div');
     const template = document.createElement('template');
     const preloader = this.createPreloader();
 
     container.classList.add(this.CSS.baseClass, this.CSS.container, this.CSS.containerLoading);
-    caption.classList.add(this.CSS.input, this.CSS.caption);
+    // caption.classList.add(this.CSS.input, this.CSS.caption);
 
     container.appendChild(preloader);
 
-    caption.contentEditable = true;
-    caption.dataset.placeholder = 'Enter a caption';
-    caption.innerHTML = this.data.caption || '';
+    // caption.contentEditable = true;
+    // caption.dataset.placeholder = 'Enter a caption';
+    // caption.innerHTML = this.data.caption || '';
 
     template.innerHTML = html;
     template.content.firstChild.setAttribute('src', this.data.embed);
@@ -150,7 +195,7 @@ export default class Embed {
     const embedIsReady = this.embedIsReady(container);
 
     container.appendChild(template.content.firstChild);
-    container.appendChild(caption);
+    // container.appendChild(caption);
 
     embedIsReady
       .then(() => {
@@ -196,6 +241,10 @@ export default class Embed {
    * @return {Service}
    */
   onPaste(event) {
+    this.performServices(event);
+  }
+
+  performServices(event) {
     const {key: service, data: url} = event.detail;
 
     const {regex, embedUrl, width, height, id = (ids) => ids.shift()} = Embed.services[service];
@@ -317,4 +366,136 @@ export default class Embed {
       observer.disconnect();
     });
   }
+
+  makeInputHolder() {
+    const inputHolder = this.make('div', this.CSS.inputHolder);
+
+    this.nodes.input = this.make('div', [this.CSS.input, this.CSS.inputEl], {
+      contentEditable: true
+    });
+
+    this.nodes.input.dataset.placeholder = this.config.placeholder || '';
+
+    this.nodes.input.addEventListener('paste', (event) => {
+      event.preventDefault();
+      event.stopPropagation();
+
+      let url;
+      if (event.type === 'paste') {
+        url = (event.clipboardData || window.clipboardData).getData('text');
+      }
+
+      this.startChecking(url);
+    });
+
+    this.nodes.input.addEventListener('keydown', (event) => {
+      const [ENTER, A] = [13, 65];
+      const cmdPressed = event.ctrlKey || event.metaKey;
+      switch (event.keyCode) {
+        case ENTER:
+          event.preventDefault();
+          event.stopPropagation();
+
+          this.startChecking(this.nodes.input.textContent);
+          break;
+        case A:
+          if (cmdPressed) {
+            // TODO
+          }
+          break;
+      }
+    });
+
+    inputHolder.appendChild(this.nodes.input);
+
+    return inputHolder;
+  }
+
+  startChecking(str) {
+    const obj = this.matchService(str);
+
+    if (obj) {
+      this.performServices(obj)
+    } else {
+      this.checkFailed('不匹配的地址')
+    }
+  }
+
+  checkFailed(errorMessage) {
+    this.api.notifier.show({
+      message: errorMessage,
+      style: 'error'
+    });
+
+    this.applyErrorStyle();
+  }
+
+  /**
+   * 输入框中输入回车或粘贴的字符串，验证是否符合预设services
+   * @type {Sting}
+   * @return {Object}
+   *
+   */
+  matchService(str) {
+    let url = str
+    const ary = str.match(/<iframe\ssrc=['|"](.+?)['|"].+iframe>$/)
+    if (ary && ary.length > 0) url = ary[1]
+
+    // match service
+    const obj = null
+    const services = Embed.services
+    for (var key in services) {
+      if (services.hasOwnProperty(key)) {
+        console.log('for in services')
+        const { regex } = services[key]
+        if (regex.test(url)) {
+          obj = this.composePasteEventMock(key, url)
+          break
+        }
+      }
+    }
+
+    return obj
+  }
+
+  /**
+   * set input error style
+   */
+  applyErrorStyle() {
+    this.nodes.inputHolder.classList.add(this.CSS.inputError);
+    // this.nodes.progress.remove();
+  }
+
+  /**
+   * remove error styles
+   */
+  removeErrorStyle() {
+    this.nodes.inputHolder.classList.remove(this.CSS.inputError);
+    // this.nodes.inputHolder.insertBefore(this.nodes.progress, this.nodes.input);
+  }
+
+  make(tagName, classNames = null, attributes = {}) {
+    const el = document.createElement(tagName);
+
+    if (Array.isArray(classNames)) {
+      el.classList.add(...classNames);
+    } else if (classNames) {
+      el.classList.add(classNames);
+    }
+
+    for (const attrName in attributes) {
+      el[attrName] = attributes[attrName];
+    }
+
+    return el;
+  }
+
+  composePasteEventMock(service, url) {
+    return {
+      detail: {
+        key: service,
+        data: url
+      }
+    }
+  };
 }
